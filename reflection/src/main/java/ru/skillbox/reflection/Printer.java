16ff0cc668693e2eb2548b2e2818aec2ff09c5a9
a-skrip <alex-himki@mail.ru>
2025-11-28 11:29:28 +0300

    создана аннотация @AccessAllowed

--- reflection/src/main/java/ru/skillbox/reflection/Printer.java
+++ reflection/src/main/java/ru/skillbox/reflection/Printer.java
@@ -1,11 +1,16 @@
 package ru.skillbox.reflection;
 
 public interface Printer {
+
+    @AccessAllowed
     void on();
 
+    @AccessAllowed
     void print(String document);
 
+    @AccessAllowed(value = false)
     void printSecret(String secretDocument);
 
+    @AccessAllowed(value = false)
     void off();
 }
