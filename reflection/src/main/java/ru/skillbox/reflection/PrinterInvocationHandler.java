0e5bbdb70ccebc511923d91e6a35455ee8910287
a-skrip <alex-himki@mail.ru>
2025-11-28 11:45:01 +0300

    Написан класс PrinterInvocationHandler + readme.md

---
+++ reflection/src/main/java/ru/skillbox/reflection/PrinterInvocationHandler.java
@@ -0,0 +1,27 @@
+package ru.skillbox.reflection;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class PrinterInvocationHandler implements InvocationHandler {
+    private final Printer orignalPrinter;
+
+    public PrinterInvocationHandler(Printer orignalPrinter) {
+        this.orignalPrinter = orignalPrinter;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        //если метод не аннотирован, то вызываем оригинальный метод интерфейса
+        if (!method.isAnnotationPresent(AccessAllowed.class)) {
+            return method.invoke(orignalPrinter, args);
+        }
+        AccessAllowed accessAllowed = method.getAnnotation(AccessAllowed.class);
+        if(accessAllowed.value()) {
+            return method.invoke(orignalPrinter, args);
+        } else {
+            System.out.println("ACCESS DENIED!");
+        }
+        return null;
+    }
+}
